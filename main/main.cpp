62f61f98281a99c7b4278d87257ef5ea476453f0
mcd5703 <mcd5703@psu.edu>
2024-12-13 04:38:51 -0500

    added outputting config defaults in main.

--- main/main.cpp
+++ main/main.cpp
@@ -42,6 +42,20 @@ int main(int argc, char *argv[])
 {
 	parseArgs(argc, argv);
 
+    // Output the values for each variable in "config.h"
+	std::cout << "-----------------------------------------------------------" << std::endl;
+	std::cout << "NOTICE: The following defaults were read through config.inp... " << std::endl;
+    std::cout << "DEFAULT_NACA: " << config::DEFAULT_NACA << std::endl;
+    std::cout << "DEFAULT_MINF: " << config::DEFAULT_MINF << std::endl;
+    std::cout << "DEFAULT_ALPHA: " << config::DEFAULT_ALPHA << std::endl;
+    std::cout << "DEFAULT_GAMINF: " << config::DEFAULT_GAMINF << std::endl;
+    std::cout << "DEFAULT_ITMAX: " << config::DEFAULT_ITMAX << std::endl;
+    std::cout << "DEFAULT_CFL: " << config::DEFAULT_CFL << std::endl;
+    std::cout << "DEFAULT_NDISP: " << config::DEFAULT_NDISP << std::endl;
+    std::cout << "DEFAULT_GRID_NJ: " << config::DEFAULT_GRID_NJ << std::endl;
+    std::cout << "DEFAULT_GRID_NK: " << config::DEFAULT_GRID_NK << std::endl;
+    std::cout << "DEFAULT_GRID_R2: " << config::DEFAULT_GRID_R2 << std::endl;
+	std::cout << "-----------------------------------------------------------" << std::endl;
 
 	return 0;
 }
